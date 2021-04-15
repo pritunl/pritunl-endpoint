a75c7782a3694e9189b5c745ad290e3438051f6d
Zachary Huff <zach.huff.386@gmail.com>
2021-04-15 01:41:21 -0400

    Add stream

---
+++ stream/stream.go
@@ -0,0 +1,44 @@
+package stream
+
+import (
+	"time"
+
+	"github.com/pritunl/mongo-go-driver/bson/primitive"
+	"github.com/sirupsen/logrus"
+)
+
+type Stream struct {
+	primary   chan *Doc
+	secondary chan *Doc
+}
+
+type Doc struct {
+	Id        primitive.ObjectID     `json:"i"`
+	Timestamp time.Time              `json:"t"`
+	Type      string                 `json:"x"`
+	Fields    map[string]interface{} `json:"f"`
+}
+
+func (s *Stream) Append(typ string, fields map[string]interface{}) {
+	doc := &Doc{
+		Id:        primitive.NewObjectID(),
+		Timestamp: time.Now(),
+		Fields:    fields,
+	}
+
+	if len(s.primary) > 1000 {
+		logrus.WithFields(logrus.Fields{
+			"length": len(s.primary),
+		}).Error("stream: Stream buffer full")
+		return
+	}
+
+	s.primary <- doc
+}
+
+func New() (strm *Stream) {
+	return &Stream{
+		primary:   make(chan *Doc, 1024),
+		secondary: make(chan *Doc, 1024),
+	}
+}
