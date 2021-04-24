5eb71d31135ef1361b9e4ddeb7fddb40b3dbf6bf
Zachary Huff <zach.huff.386@gmail.com>
2021-04-24 02:19:15 -0400

    Init logger in main

--- main.go
+++ main.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"github.com/pritunl/pritunl-endpoint/input"
+	"github.com/pritunl/pritunl-endpoint/logger"
 	"github.com/pritunl/pritunl-endpoint/system"
 )
 
 func main() {
+	logger.Init()
+
 	system.Register()
 
 	input.Run()
