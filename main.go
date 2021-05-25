3b082124b1c97d5f678c576d8647abf845c93c4e
Zachary Huff <zach.huff.386@gmail.com>
2021-05-24 23:31:45 -0400

    Add help to main

--- main.go
+++ main.go
@@ -1,7 +1,12 @@
 package main
 
 import (
+	"flag"
+	"fmt"
+	"time"
+
 	"github.com/pritunl/pritunl-endpoint/config"
+	"github.com/pritunl/pritunl-endpoint/constants"
 	"github.com/pritunl/pritunl-endpoint/disk"
 	"github.com/pritunl/pritunl-endpoint/endpoint"
 	"github.com/pritunl/pritunl-endpoint/input"
@@ -11,23 +16,58 @@ import (
 	"github.com/pritunl/pritunl-endpoint/system"
 )
 
+const help = `
+Usage: pritunl-endpoint COMMAND
+
+Commands:
+  version   Show version
+  start     Start endpoint service
+  register  Get default administrator password
+`
+
 func main() {
+	defer time.Sleep(500 * time.Millisecond)
+
+	flag.Parse()
+
 	logger.Init()
 
-	err := config.Init()
-	if err != nil {
-		panic(err)
+	switch flag.Arg(0) {
+	case "start":
+		err := config.Init()
+		if err != nil {
+			panic(err)
+		}
+
+		err = endpoint.Init()
+		if err != nil {
+			panic(err)
+		}
+
+		system.Register()
+		load.Register()
+		disk.Register()
+		network.Register()
+
+		input.Run()
+
+		return
+	case "version":
+		fmt.Printf("pritunl-endpoint v%s\n", constants.Version)
+		return
+	case "register":
+		err := config.Init()
+		if err != nil {
+			panic(err)
+		}
+
+		err = endpoint.RegisterCmd()
+		if err != nil {
+			panic(err)
+		}
+
+		return
 	}
 
-	err = endpoint.Init()
-	if err != nil {
-		panic(err)
-	}
-
-	system.Register()
-	load.Register()
-	disk.Register()
-	network.Register()
-
-	input.Run()
+	fmt.Println(help)
 }
