da4e5a6e2bd9cd7b6775762badc1353ee886cc83
Zachary Huff <zach.huff.386@gmail.com>
2022-09-19 17:54:28 -0400

    Add startup to input

--- input/input.go
+++ input/input.go
@@ -10,6 +10,7 @@ import (
 type Input struct {
 	Name     string
 	Rate     time.Duration
+	Startup  func(stream *stream.Stream) error
 	Handler  func(stream *stream.Stream) error
 	timstamp time.Time
 }
@@ -18,8 +19,23 @@ func Run() {
 	strm := stream.New()
 	go strm.Run()
 
+	for _, in := range inputs {
+		if in.Startup != nil {
+			err := in.Startup(strm)
+			if err != nil {
+				logrus.WithFields(logrus.Fields{
+					"error": err,
+				}).Error("input: Input startup error")
+			}
+		}
+	}
+
 	for {
 		for _, in := range inputs {
+			if in.Handler == nil {
+				continue
+			}
+
 			if time.Since(in.timstamp) > in.Rate {
 				in.timstamp = time.Now()
 
