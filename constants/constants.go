efee6ec833df7e2ddef64b8f173f91bef1132036
Zachary Huff <zach.huff.386@gmail.com>
2022-09-23 21:54:13 -0400

    Remove comment from constants

--- constants/constants.go
+++ constants/constants.go
@@ -9,30 +9,3 @@ var (
 	ConfPath    = "/etc/pritunl-endpoint.json"
 	VarDir      = "/var/lib/pritunl-endpoint"
 )
-
-//timestamp := time.Now().UTC().Add(-2160 * time.Hour)
-//cpuUsage = 30.0
-//memUsage = 50.0
-//
-//for i := 0; i < 1296000; i++ {
-//timestamp = timestamp.Add(1 * time.Minute)
-//cpuUsage = utils.RandFloatData(cpuUsage, 10, 20, 80, 90, 1)
-//memUsage = utils.RandFloatData(memUsage, 10, 20, 80, 90, 1)
-//
-//doc := &System{
-//Timestamp: timestamp,
-//CpuUsage:  cpuUsage,
-//MemTotal:  memTotal,
-//MemUsage:  memUsage,
-//SwapTotal: swapTotal,
-//SwapUsage: swapUsage,
-//}
-//
-//stream.Append(doc)
-//}
-//
-//println("***************************************************")
-//println("done")
-//println("***************************************************")
-//
-//time.Sleep(1000000 * time.Second)
